cb2d3d01b7d574a891b74c923e1f7fc2ec3b675a
rinn-code <98275792+rinn-code@users.noreply.github.com>
2022-11-16 20:34:37 -0600

    TODO 3 Complete

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,13 +1,16 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.Job;
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
+import static org.launchcode.techjobs.mvc.controllers.ListController.tableChoices;
 
 
 /**
@@ -24,25 +27,29 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @PostMapping
-    public String displaySearchResults(String searchType, String searchTerm, Model model) {
-        model.addAttribute("searchType", searchType);
-        model.addAttribute("searchTerm", searchTerm);
-        return("search");
+    @PostMapping(value="results")
+    public String displaySearchResults(Model model, @RequestParam String searchType,@RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+        if (searchTerm.equals("all")  || searchTerm.equals("")) {
+            jobs = JobData.findAll();
+            model.addAttribute("jobs", jobs);
+            model.addAttribute("columns", columnChoices);
+            return "search";
+        }
+       else{
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("jobs", jobs);
+            model.addAttribute("columns", columnChoices);
+            return "search";
+        }
 
 
-//    <ol th:if = "${coffeeOptions.size() > 1}">
-//   <li th:each="item : ${coffeeOptions}" th:text="${item}"></li>
-//</ol>
-//
-//<h2 th:if = '${userSelection.equals("instant")}'>You can do better!</h2>
+//model.addAttribute(ListController.columnChoices.get(searchType));
+//        model.addAttribute(ListController.tableChoices.get(category.value()));
+//        model.addAttribute("searchTerm", tableChoices);
+//        model.addAttribute("beep", JobData.findByColumnAndValue(searchType, searchTerm));
 
-//    If the user enters “all” in the search box, or if they leave the box empty, call the findAll() method from JobData.
-//    Otherwise, send the search information to findByColumnAndValue.
-//    In either case, store the results in a jobs ArrayList.
-//    Pass jobs into the search.html view via the model parameter.
-//    Pass ListController.columnChoices into the view, as the existing search handler does
-
+ //       return "search";
    }
 
 }
